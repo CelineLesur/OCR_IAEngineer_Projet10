9829791bbf976c991a455b5f60fe462175109e56
CelineLesur <141427131+CelineLesur@users.noreply.github.com>
2025-10-10 11:51:37 +0200

    Update function_app.py

--- function_app.py
+++ function_app.py
@@ -1,4 +1,9 @@
-import azure.functions as func
+import azure.functions as func 
+import logging 
+import json 
+import pickle 
+import pandas as pd 
+from azure.storage.blob import BlobServiceClient 
 
 app = func.FunctionApp()
 
